785dd7a8850c70e58989c52eb0d942f503e587b7
infernalslam <infernal-slam@hotmail.com>
2018-03-07 11:15:41 +0700

    fix code

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'onsenui/css/onsenui.css'
+import 'onsenui/css/onsen-css-components.css'
 
 Vue.config.productionTip = false
 
